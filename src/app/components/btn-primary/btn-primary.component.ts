08b4546617de819d1cf59c463c6b621f6b3d2036
thihuli <thiagocandidof@hotmail.com>
2024-02-03 17:04:44 -0300

    chore: finished button component and cta section

--- src/app/components/btn-primary/btn-primary.component.ts
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -4,14 +4,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 type BtnVariant = "primary" | "sencondary"
 
 @Component({
-  selector: 'app-btn-primary',
+  selector: 'btn-primary',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './btn-primary.component.html',
   styleUrl: './btn-primary.component.scss'
 })
 export class BtnPrimaryComponent {
-  @Input("btn-text") text: string = "";
+  @Input("btn-text") btnText: string = "";
   @Input() disabled: boolean = false;
   @Input() loading: boolean = false;
   @Input() variant: BtnVariant = "primary";
